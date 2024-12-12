71042601eb4fa0e8d0a35e7214a1d3b6919403aa
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    thyme: Implement DSPVolumeSynchronizer
    * thyme has a speaker that needs framework to co-operate with dsp to synchronize volume so that the dsp can limit bass when the volume is high to prevent distortion

--- device.mk
+++ device.mk
@@ -32,6 +32,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,qdcm_calib_data_*.xml,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc/)
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_umi)
+
+# DSP Volume Synchronizer
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
