ecc442cc44ccf72b2385c56751062b5136b72659
gpablo1 <gpablo1@yahoo.com.ar>
2025-05-24 12:57:13 +0200

    Reorder lines file and ship Google Camera

--- lineage_umi.mk
+++ lineage_umi.mk
@@ -14,22 +14,31 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from umi device
 $(call inherit-product, device/xiaomi/umi/device.mk)
 
-# Extra Tags
-TARGET_HAS_UDFPS := true
-WITH_GMS := true
+# Pixel features
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
 TARGET_INCLUDE_GOOGLE_DIALER := true
-BUILD_BCR := true
+WITH_GMS := true
 TARGET_CORE_GMS := true
 TARGET_CORE_GMS_EXTRAS := true
 TARGET_GMS_EXTRAS := true
 WITH_PIXEL_OVERLAYS := true
+# Ship Google Camera
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+
+# Extra Tags
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+BUILD_BCR := true
 TARGET_DISABLE_EPPE := true
 TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device Details
 PRODUCT_NAME := lineage_umi
-# PRODUCT_NAME := rising_umi
 PRODUCT_DEVICE := umi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -39,27 +48,22 @@ RISING_BATTERY := 4780mah
 RISING_STORAGE := 256gb
 RISING_RAM := 8gb
 RISING_DISPLAY := 2340×1080
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-RISING_CHIPSET := "Snapdragon 865 5G"
-RISING_MAINTAINER := "gpablo1"
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+RISING_PACKAGE_TYPE := GAPPS
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_ENABLE_BLUR := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-
+RISING_CHIPSET := "Snapdragon 865 5G"
 PRODUCT_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer
+RISING_MAINTAINER := "gpablo1"
+
 # Keys
 -include vendor/lineage-priv/keys/keys.mk
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Rising specific prop overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
