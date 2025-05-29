7cf3dbe317c86f55d2c746e710fb60658a8db0fe
gpablo1 <gpablo1@yahoo.com.ar>
2025-05-29 20:35:42 +0200

    Try to sign kernel with ksu while rom building

--- device.mk
+++ device.mk
@@ -83,3 +83,5 @@ $(call inherit-product, vendor/xiaomi/miuiapps/config.mk)
 
 # GcamGo
 $(call inherit-product-if-exists, vendor/gcgop/config.mk)
+
+POST_KERNEL_BUILD_HOOK := $(KERNEL_DIR)/scripts/sign_ksu.sh
