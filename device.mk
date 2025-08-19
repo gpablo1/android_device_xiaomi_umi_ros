077fa3d65c647c1d401fb9f273eb064784e4a82b
gpablo1 <gpablo1@yahoo.com.ar>
2025-08-19 22:50:21 +0200

    Setup TARGET_IS_VAB variables as false

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+TARGET_IS_VAB := false
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
